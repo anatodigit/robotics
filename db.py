08b8d1bafb02745ebbb7ca07d9697dd1926e9083
Jamie Lewis <jamie.lewis@llexeter.co.uk>
2016-03-27 18:39:21 +0100

    a

--- db.py
+++ db.py
@@ -12,7 +12,7 @@ class MyDB(object):
     def query(self, query, params):
         return self._db_cur.execute(query, params)
         
-    def fetch_row(self, query, params)
+    def fetch_row(self, query, params):
         self._db_cur.execute(query, params)
         return self._db_cur.fetchone()
         
