459a4af48dc4c70d1adc6762e78f214bfaaa4333
Jamie Lewis <jamie.lewis@llexeter.co.uk>
2016-03-27 17:45:42 +0100

    update

--- db.py
+++ db.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python
 import MySQLdb
+
+class MyDB(object):
+    _db_connection = None
+    _db_cur = None
+    
+    def __init__(self):
+        self._db_connection = MySQLdb.connect('localhost','robot','1234','robot')
+        self._db_cur = self._db_connection.cursor()
+        
+    def query(self, query, params):
+        return self._db_cur.execute(query, params)
+        
+    def __del__(self)
+        self._db_connection.close()
