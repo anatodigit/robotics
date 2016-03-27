caec5af8f206dbed64e3dfa7413754af7e193d11
Jamie Lewis <jamie.lewis@llexeter.co.uk>
2016-03-27 17:48:18 +0100

    fix

--- db.py
+++ db.py
@@ -12,5 +12,5 @@ class MyDB(object):
     def query(self, query, params):
         return self._db_cur.execute(query, params)
         
-    def __del__(self)
+    def __del__(self):
         self._db_connection.close()
