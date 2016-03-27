a58d8aaf4eef74036e16d4ec5c8279016f235c90
Jamie Lewis <jamie.lewis@llexeter.co.uk>
2016-03-27 18:26:46 +0100

    a

--- motors.py
+++ motors.py
@@ -2,7 +2,7 @@ import RPi.GPIO as gpio
 import time
 
 def init():
-    gpio.getmode(gpio.BOARD)
+    gpio.setmode(gpio.BOARD)
     gpio.setup(7,gpio.OUT)
     gpio.setup(11,gpio.OUT)
     gpio.setup(13,gpio.OUT)
