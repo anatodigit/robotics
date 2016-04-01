a0ea7f172623ccb6a1d298fbcf42434cc47a1a1f
Jamie Lewis <jamie.lewis@llexeter.co.uk>
2016-04-01 23:11:39 +0100

    try again

--- LPS25H.py
+++ LPS25H.py
@@ -2,13 +2,10 @@
 from smbus import SMBus
 busNum = 1
 b = SMBus(busNum)
-LSM = 0x1d
-LSM_WHOAMI = 0b1011101 #Device self-id
+PTS = 0x5d #Device I2C slave address
+PTS_WHOAMI = 0b1011101 #Device self-id
 
-print 'test:'+str(b.read_byte_data(LSM, 0x0f))
-
-
-if b.read_byte_data(LSM, 0x0f) == LSM_WHOAMI:
+if b.read_byte_data(PTS, 0x0f) == PTS_WHOAMI:
     print 'LPS25H detected successfully.'
 else:
     print 'No LPS25H detected on bus '+str(busNum)+'.'
