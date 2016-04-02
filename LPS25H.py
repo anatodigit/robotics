4b170a3580c2b0e8b87900e8af9a5da94baf7c1d
Jamie Lewis <jamie.lewis@llexeter.co.uk>
2016-04-02 09:54:26 +0100

    a

--- LPS25H.py
+++ LPS25H.py
@@ -5,6 +5,10 @@ b = SMBus(busNum)
 PTS = 0x5d #Device I2C slave address
 PTS_WHOAMI = 0b1011101 #Device self-id
 
+(chip_id, version) = b.read_i2c_block_data(PTS, 0xD0, 2)
+print "Chip Id:", chip_id, "Version:", version
+
+
 if b.read_byte_data(PTS, 0x0f) == PTS_WHOAMI:
     print 'LPS25H detected successfully.'
 else:
